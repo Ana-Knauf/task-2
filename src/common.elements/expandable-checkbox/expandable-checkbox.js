b41d4fc6f2cae25599bba07231c0a89652cd4048
Anastasia <svch9039@gmail.com>
2020-08-12 17:17:07 +0200

    added the mark to the button

--- src/common.elements/expandable-checkbox/expandable-checkbox.js
+++ src/common.elements/expandable-checkbox/expandable-checkbox.js
@@ -1,5 +1,8 @@
 let coll = document.getElementsByClassName("expand-checkbox-button");
 let i;
+let angle = window.getComputedStyle(
+	document.querySelector('.expand-checkbox-button'), ':after'
+).getPropertyValue('trasform');
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
@@ -9,6 +12,11 @@ for (i = 0; i < coll.length; i++) {
       content.style.display = "none";
     } else {
       content.style.display = "block";
+    }
+    if (angle === "rotate(45deg)") {
+      angle = "rotate(225deg)";
+    } else {
+      angle = "rotate(45deg)";
     }
   });
 }
