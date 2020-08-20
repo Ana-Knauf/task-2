ad62dbe8b8470a65535023e098d5a267be577109
Anastasia <svch9039@gmail.com>
2020-08-20 20:17:47 +0200

    added new element date-dropdown

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./js/common";
 import "./scss/main.scss";
 import "jquery";
 import "./common.elements/dropdown-guests/dropdown-guests";
+import "./common.elements/date-dropdown/date-dropdown"
