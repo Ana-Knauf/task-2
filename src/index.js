5709725bae7856d13a7456f5617b5f6fa654f49a
Anastasia <svch9039@gmail.com>
2020-08-28 18:49:20 +0200

    new element range slider

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import "./js/common";
 import "./scss/main.scss";
 import "jquery";
 import "./common.elements/dropdown-guests/dropdown-guests";
-import "./common.elements/date-dropdown/date-dropdown"
+import "./common.elements/date-dropdown/date-dropdown";
+// import "./common.elements/range-slider/range-slider";
