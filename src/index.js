fb8ab191d354754f4e4caefd91523228cc6107ee
Anastasia <svch9039@gmail.com>
2020-08-28 19:35:21 +0200

    finished element range slider (+ jquery ui)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./js/common";
-import "./scss/main.scss";
 import "jquery";
+require('webpack-jquery-ui/slider');
+import "./scss/main.scss";
 import "./common.elements/dropdown-guests/dropdown-guests";
 import "./common.elements/date-dropdown/date-dropdown";
-// import "./common.elements/range-slider/range-slider";
+import "./common.elements/range-slider/range-slider";
