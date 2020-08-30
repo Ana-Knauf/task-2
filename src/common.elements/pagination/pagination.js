5dc724eaad5e562d49804ff7f8887b8d5c0a1aab
Anastasia <svch9039@gmail.com>
2020-08-30 23:08:58 +0200

    new elements pagination and room details

---
+++ src/common.elements/pagination/pagination.js
@@ -0,0 +1,9 @@
+$("#pagination-demo").pagination({
+    // options here
+    pageIndex: 0,
+    pageSize: 12,
+    total: 200,
+    showInfo: true,
+    pageElementSort: ['$page','$size','$jump','$info'],
+    pageBtnCount: 6,
+  });
--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ import "./scss/main.scss";
 import "./common.elements/dropdown-guests/dropdown-guests";
 import "./common.elements/date-dropdown/date-dropdown";
 import "./common.elements/range-slider/range-slider";
+import "./common.elements/pagination/mricode.pagination";
+import "./common.elements/pagination/pagination";
